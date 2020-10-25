cec8b7754f7b6102acdc7df5849ff193489f324f
SaiPradeepti <saipradeepti14@gmail.com>
2020-10-25 18:53:33 +0530

    adding ui.js

---
+++ ui.js
@@ -0,0 +1,51 @@
+// UI Controller
+const UICtrl = (function(){
+    const UISelectors = {
+        itemList: '#item-list',
+        addBtn: '.add-btn',
+        itemNameInput: '#item-name',
+        itemCaloriesInput: '#item-calories'
+    }
+    
+    return {
+        populateItemList: function(items){
+            let html = '';
+
+            items.forEach(function(item){
+                html += `<li class="collection-item" id="item-${item.id}">
+                <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+                <a href="#" class="secondary-content">
+                <i class="edit-item fas fa-edit"></i>
+                </a>
+              </li>`;
+            });
+
+            //Insert list items
+            document.querySelector(UISelectors.itemList).innerHTML = html;
+        },
+        getItemInput: function(){
+            return {
+                name: document.querySelector(UISelectors.itemNameInput).value,
+                calories: document.querySelector(UISelectors.itemCaloriesInput).value
+            }
+        },
+        addListItem: function(item){
+            //Create li element
+            const li = document.createElement('li');
+            //Add class
+            li.className = 'collection-item';
+            //Add ID
+            li.id = `item-${item.id}`;
+            //Add HTML
+            li.innerHTML = `<strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+            <a href="#" class="secondary-content">
+            <i class="edit-item fas fa-edit"></i>
+            </a>`;
+            //Insert item
+            document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li);
+        },
+        getSelectors: function(){
+            return UISelectors;
+        }
+    }
+})();
