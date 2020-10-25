88018170171175d07a628489ca75e685932433b5
SaiPradeepti <saipradeepti14@gmail.com>
2020-10-25 18:53:13 +0530

    adding item.js

---
+++ item.js
@@ -0,0 +1,48 @@
+// Item Controller
+const ItemCtrl = (function(){
+    //Item Constructor
+    const Item = function(id, name, calories){
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    }
+
+    //Data Structure / State
+    const data = {
+        items: [
+            {id: 0, name: 'Steak Dinner', calories: 1200},
+            {id: 1, name: 'Cookie', calories: 400},
+            {id: 2, name: 'Eggs', calories: 300}
+        ],
+        currentItem: null,
+        totalCalories: 0
+    }
+
+    return {
+        getItems: function(){
+            return data.items;  
+        },
+        addItem: function(name, calories){
+            let ID;
+            if(data.items.length > 0){
+                ID = data.items[data.items.length - 1].id + 1;
+            }else{
+                ID = 0;
+            }
+
+            //Calories to number
+            calories = parseInt(calories);
+
+            //Create new Item
+            newItem = new Item(ID, name, calories);
+
+            //Add to items array
+            data.items.push(newItem);
+
+            return newItem;
+        },
+        logData: function(){
+            return data;
+        }
+    }
+})();
